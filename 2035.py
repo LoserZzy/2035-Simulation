e272c39bfbcb5f09ff3083f3d8061a3da70ba189
Just_Zzy <34079701+LoserZzy@users.noreply.github.com>
2022-04-18 09:53:28 +0800

    Create 2035.py

---
+++ 2035.py
@@ -0,0 +1,422 @@
+# coding=gbk
+from calendar import c
+import os, sys
+def clear():os.system('cls')
+
+
+def showtext(textid):
+    clear()
+    path = './texts/' + textid + '.txt'
+    f = open(path, 'r', encoding='utf-8')
+    text = f.read()
+    print(text)
+    return
+
+def chapter1():
+    while 1:
+        showtext('1')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2') 
+            input()
+
+def chapter2():
+    while 1:
+        showtext('2')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter3():
+    while 1:
+        showtext('3')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter4():
+    while 1:
+        showtext('4')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        elif a == '3':
+            return 3
+        else:
+            print('请输入 1 或者 2 或者 3')
+            input()
+
+def chapter5():
+    while 1:
+        showtext('5')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter6():
+    while 1:
+        showtext('6')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')   
+            input()         
+
+def chapter7():
+    while 1:
+        showtext('7')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter8():
+    while 1:
+        showtext('8')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter9():
+    while 1:
+        showtext('9')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter10():
+    while 1:
+        showtext('10')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter11():
+    while 1:
+        showtext('11')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter12():
+    while 1:
+        showtext('12')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter13():
+    while 1:
+        showtext('13')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')   
+            input()     
+
+def chapter14():
+    while 1:
+        showtext('14')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter15():
+    while 1:
+        showtext('15')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter16():
+    while 1:
+        showtext('16')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter17():
+    while 1:
+        showtext('17')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter18():
+    while 1:
+        showtext('18')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter19():
+    while 1:
+        showtext('19')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        elif a == '3':
+            return 3
+        else:
+            print('请输入 1 或者 2 或者 3')
+            input()
+
+def chapter20():
+    while 1:
+        showtext('20')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter21():
+    while 1:
+        showtext('21')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter22():
+    while 1:
+        showtext('22')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter23():
+    while 1:
+        showtext('23')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+def chapter24():
+    while 1:
+        showtext('24')
+        print('您已经走到了本分支的结局，您可以选择：1.重新游玩    2.退出')
+        a = input()
+        if a == '1':
+            return 1
+        elif a == '2':
+            return 2
+        else:
+            print('请输入 1 或者 2')
+            input()
+
+
+
+if __name__ == "__main__":
+    print('                ——————————————声明——————————————\n        欢迎来到2035模拟器，这是由Zzy原创的互动小说，仅供Zzy的亲朋好友进行游玩，请不要在未经原作者允许的情况下私自传播，或者将文章内容断章取义进行扩散。如果您能接受这些要求，请输入 1 ，开始体验。')
+    while 1:
+        a = input()
+        if a == '1':
+            print('        感谢理解与支持！！')
+            break
+        print('        您不认同吗？那么您无法体验Zzy的互动小说，还请理解。')
+
+    while 1:
+        a = chapter1()
+        if a == 1:
+            a = chapter2()
+            if a == 1:
+                a = chapter3()
+                if a == 1:
+                    a = chapter4()
+                    if a == 1:
+                        a = chapter5()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                    elif a == 2:
+                        a = chapter6()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                    else:
+                        a = chapter7()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                else:
+                    a = chapter8()
+                    if a == 1:
+                        a = chapter9()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                    else:
+                        a = chapter10()
+                        if a == 1:
+                            continue
+                        sys.exit()
+            else:
+                a = chapter11()
+                if a == 1:
+                    a = chapter12()
+                    if a == 1:
+                        a = chapter13()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                    else:
+                        a = chapter14()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                else:
+                    a = chapter15()
+                    if a == 1:
+                        a = chapter16()
+                        if a == 1:
+                            continue
+                        sys.exit()
+                    else:
+                        a = chapter17()
+                        if a == 1:
+                            continue
+                        sys.exit()
+        else:
+            a = chapter18()
+            if a == 1:
+                a = chapter19()
+                if a == 1:
+                    a = chapter20()
+                    if a == 1:
+                        continue
+                    sys.exit()
+                elif a == 2:
+                    a = chapter21()
+                    if a == 1:
+                        continue
+                    sys.exit()
+                else:
+                    a = chapter22()
+                    if a == 1:
+                        continue
+                    sys.exit()
+            else:
+                a = chapter23()
+                if a == 1:
+                    a = chapter24()
+                    if a == 1:
+                        continue
+                    sys.exit()
+                else:
+                    a = chapter13()
+                    if a == 1:
+                        continue
+                    sys.exit()
